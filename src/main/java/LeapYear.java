b9796e2195f161cb757c7b7803d0e37cdea32fea
Jeffrey Ruder <ruderjt@gmail.com>
2015-12-31 11:22:20 -0800

    Change code to pass multiple of four hundred test

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -2,7 +2,9 @@ public class LeapYear {
   public static void main(String[] args) {}
 
   public Boolean isLeapYear(Integer year) {
-    if (year % 100 == 0) {
+    if (year % 400 == 0) {
+      return true;
+    } else if (year % 100 == 0) {
       return false;
     } else {
       return year % 4 == 0;
